35643aedce49098a3252e1baa23bbf8e29814e1a
1PKFilms <gpsebernicke@gmail.com>
2013-04-22 00:54:00 +0200

    Added a temproarly very cheap Command to protect (automaticly protects from command senders posto command sender y -10 commandsenders x +10 (not the dirction in that you are looking just + 10 blocks) and z+10 blocks)

--- common/Enderware/common/Command.java
+++ common/Enderware/common/Command.java
@@ -3,13 +3,16 @@ package Enderware.common;
 import java.util.List;
 
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import Enderware.api.IEnderCommand;
+import Enderware.protection.ProtectionManger;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
 public class Command{
 	  public static void initCommands(FMLServerStartingEvent event) {
 		 
 	        event.registerServerCommand(new TestCommand() );
+	        event.registerServerCommand(new ProtectCommand());
 	    }
 }
 class TestCommand extends IEnderCommand{
@@ -64,4 +67,63 @@ class TestCommand extends IEnderCommand{
 		return "test";
 	}
 	
+}
+class ProtectCommand extends IEnderCommand{
+
+    @Override
+    public String getCommandName() {
+        // TODO Auto-generated method stub
+        return "protect";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender var1) {
+        // TODO Auto-generated method stub
+        return "protect";
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public List getCommandAliases() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void processCommand(ICommandSender var1, String[] var2) {
+     
+        var1.sendChatToPlayer("Protecting that can take a while..");
+        EntityPlayer player = getCommandSenderAsPlayer(var1);
+        long start = System.currentTimeMillis();
+        String[] friends = {};
+        ProtectionManger.instance.protect(player, player.posX, player.posY, player.posZ, player.posX+10, player.posY-10, player.posZ+10, player.dimension, false, 10, friends);
+        long end = System.currentTimeMillis();
+        player.sendChatToPlayer("Protecting finished after "+(start-end)+"mil");
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public List addTabCompletionOptions(ICommandSender var1, String[] var2) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameIndex(int var1) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getPermisssion() {
+        // TODO Auto-generated method stub
+        return "make_Protections";
+    }
+
 }
